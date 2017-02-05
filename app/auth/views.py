7a2bda2fbd1ba2bb7695569a5dc562d18f84d54e
Nitin Prakash <prakash.nitin63@gmail.com>
2017-02-05 23:43:04 +0530

    add homepage functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
-from flask import flash, redirect,render_template, url_for
-from flask_login import login_user, logout_user, login_required
+from flask import abort, flash, redirect,render_template, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from . import auth
 from forms import LoginForm, RegistrationForm
 from .. import db
@@ -53,3 +53,22 @@ def logout():
 	flash('You have successfully been logged out.')
 
 	return redirect(url_for('auth.login'))
+
+@home.route('/')
+	def homepage():
+		return render_template('home/index.html', title="Welcome")
+
+
+@home.route('/dashboard')
+@login_required
+def dashboard():
+	return render_template('home/dashboard.html', title="Dashboard")
+
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+	# prevent non-admins from acessing the page
+	if not current_user.is_admin:
+		abort(403)
+
+	return render_template('home/admin_dashboard.html', title="Dashboard")
