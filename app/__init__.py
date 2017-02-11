2674f7618319c13eaa7f7bb20f181597d06e6eb4
Nitin Prakash <prakash.nitin63@gmail.com>
2017-02-11 16:09:44 +0530

    remove route, fix keyerror

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 # local imports
@@ -7,14 +9,20 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+
 def create_app(config_name):
 	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_object(app_config[config_name])
+	app.config.from_object(app_config)
 	app.config.from_pyfile('config.py')
 	db.init_app(app)
 
-	@app.route('/')
-	def hello_world():
-		return 'Hello, World!'
+	login_manager.init_app(app)
+	login_manager.login_message = "You must be logged in"
+	login_manager.login_view = "auth.login"
+
+	migrate = Migrate(app, db)
+
+	from app import models
 
 	return app
