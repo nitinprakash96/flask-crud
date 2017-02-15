4b0d380a262341c8b2aad9a00ab09ac2de07a335
Nitin Prakash <prakash.nitin63@gmail.com>
2017-02-15 23:23:15 +0530

    fix syntax

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -20,11 +20,11 @@ class RegistrationForm(FlaskForm):
 	submit = SubmitField('Register')
 	
 	def validate_email(self, field):
-		if Employee.query.filter_by(email=field.data).first()
+		if Employee.query.filter_by(email=field.data).first():
 			raise ValidationError('Email is already in use')
 
 	def validate_username(self, field):
-		if Employee.query.filter_by(username=field.data).first()
+		if Employee.query.filter_by(username=field.data).first():
 			raise ValidationError('Username is already in use')
 
 
